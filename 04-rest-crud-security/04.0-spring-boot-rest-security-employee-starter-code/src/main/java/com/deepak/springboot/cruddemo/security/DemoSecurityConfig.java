5b2fb934b71aed5bbdbb848f2d4c2846217ea8de
DeepakEuler <deepaksharmaforwork@gmail.com>
2024-05-12 23:28:14 +0530

    added restricted urls with role specific and general

--- 04-rest-crud-security/04.0-spring-boot-rest-security-employee-starter-code/src/main/java/com/deepak/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 04-rest-crud-security/04.0-spring-boot-rest-security-employee-starter-code/src/main/java/com/deepak/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.deepak.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -30,6 +34,26 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, marry, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "api/employees/**").hasRole("ADMIN")
+                ));
+        //use http basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        //disable csrf
+        //in general, not required for stateless rest APIs that uses POST, PUT, DELETE and GET
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 
 }
