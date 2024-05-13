2385cbde7a293b4c475824b43d11dcda3fb55c77
DeepakEuler <deepaksharmaforwork@gmail.com>
2024-05-13 15:58:57 +0530

    added spring mvc project

---
+++ 05-spring-boot-spring-mvc/05.0-thymeleaf-demo-helloworld/src/main/java/com/deepak/springboot/thymelafdemo/ThymelafdemoApplication.java
@@ -0,0 +1,13 @@
+package com.deepak.springboot.thymelafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymelafdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymelafdemoApplication.class, args);
+	}
+
+}
